359339dbe0a1a0da0523f15a41b3d12a982217a6
Zombieland4942 <zombieland4942@hotmail.com>
2020-05-15 19:08:09 +1000

    Added Weapons Group Added Subgroup for Assault weapons

--- prototypes/groups/groups.lua
+++ prototypes/groups/groups.lua
@@ -19,6 +19,19 @@ data:extend(
     name = "ds-raw-material",
     group = "darkstar-core",
     order = "b"
+  },
+  {
+    type = "item-group",
+    name = "darkstar-weapons",
+    order = "g",
+    icon = "__darkstar-core__/graphics/groups/darkstar2.png",
+    icon_size = 256
+  },
+  {
+    type = "item-subgroup",
+    name = "ds-assult",
+    group = "darkstar-weapons",
+    order = "a"
   }
 }
 )
