f6c51ae974141e082679bdc7649011054f4339ea
Zombieland4942 <zombieland4942@hotmail.com>
2020-05-16 08:21:16 +1000

    - moved item groups from core to individual mods

--- prototypes/groups/groups.lua
+++ prototypes/groups/groups.lua
@@ -5,7 +5,7 @@ data:extend(
     type = "item-group",
     name = "darkstar-core",
     order = "f",
-    icon = "__darkstar-core__/graphics/groups/darkstar1.png",
+    icon = "__darkstar-weapons__/graphics/groups/darkstar-weapon.png",
     icon_size = 256
   },
   {
@@ -18,25 +18,6 @@ data:extend(
     type = "item-subgroup",
     name = "ds-raw-material",
     group = "darkstar-core",
-    order = "b"
-  },
-  {
-    type = "item-group",
-    name = "darkstar-weapons",
-    order = "g",
-    icon = "__darkstar-core__/graphics/groups/darkstar2.png",
-    icon_size = 256
-  },
-  {
-    type = "item-subgroup",
-    name = "ds-assult",
-    group = "darkstar-weapons",
-    order = "a"
-  },
-  {
-    type = "item-subgroup",
-    name = "ds-sniper",
-    group = "darkstar-weapons",
     order = "b"
   }
 }
