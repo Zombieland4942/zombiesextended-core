593a5ae06ba7e52fafc467312243dfa68b647ec7
Zombieland4942 <zombieland4942@hotmail.com>
2020-05-16 15:00:13 +1000

    moved groups into core mod

--- prototypes/groups/groups.lua
+++ prototypes/groups/groups.lua
@@ -1,24 +1,55 @@
   
 data:extend(
-{
-  {
-    type = "item-group",
-    name = "darkstar-core",
-    order = "f",
-    icon = "__darkstar-core__/graphics/groups/darkstar1.png",
-    icon_size = 256
-  },
-  {
-    type = "item-subgroup",
-    name = "ds-raw-resource",
-    group = "darkstar-core",
-    order = "a"
-  },
-  {
-    type = "item-subgroup",
-    name = "ds-raw-material",
-    group = "darkstar-core",
-    order = "b"
-  }
-}
+    {
+        {
+            type = "item-group",
+            name = "darkstar-core",
+            order = "f",
+            icon = "__darkstar-core__/graphics/groups/darkstar-core.png",
+            icon_size = 256
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-raw-resource",
+            group = "darkstar-core",
+            order = "a"
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-raw-material",
+            group = "darkstar-core",
+            order = "b"
+        },
+        {
+            type = "item-group",
+            name = "darkstar-weapons-equipment",
+            order = "g",
+            icon = "__darkstar-core__/graphics/groups/darkstar-weapon-equipment.png",
+            icon_size = 256
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-assult",
+            group = "darkstar-weapons-equipment",
+            order = "a"
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-sniper",
+            group = "darkstar-weapons-equipment",
+            order = "b"
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-railgun",
+            group = "darkstar-weapons-equipment",
+            order = "c"
+        },
+        {
+            type = "item-subgroup",
+            name = "ds-rockets",
+            group = "darkstar-weapons-equipment",
+            order = "c"
+        }
+    }
 )
